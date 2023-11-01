d5bca2b5524f4c18505fcbdd767b7203d73fe716
nemo <fred.ferreira.a97@gmail.com>
2023-11-01 09:54:26 -0300

    part 1.3

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -13,8 +13,7 @@ const Part = (props) => {
     </p>
     )
 }
-const a == 1:
- 
+
 const Content = (props) => {
     return(
 	<div>
@@ -39,26 +38,31 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
   return (
     <div>
 	<Hello course = {course}/>
-	<Content part1 = {part1}
-		 exercises1 = {exercises1}
-		 part2 = {part2}
-		 exercises2 = {exercises2}
-		 part3 = {part3}
-		 exercises3 = {exercises3}/>
+	<Content part1 = {part1.name}
+		 exercises1 = {part1.exercises}
+		 part2 = {part2.name}
+		 exercises2 = {part2.exercises}
+		 part3 = {part3.name}
+		 exercises3 = {part3.exercises}/>
 	
-	<Total p1 = {exercises1}
-	       p2 = {exercises2}
-	       p3 = {exercises3}/>
+	<Total p1 = {part1.exercises}
+	       p2 = {part2.exercises}
+	       p3 = {part3.exercises}/>
     </div>
   )
 }
